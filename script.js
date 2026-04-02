2c8fa883f6aba072c8128bb0a2c101ad88d91717
Grace Ma <54190353+gracewqma@users.noreply.github.com>
2026-04-02 15:29:06 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const board = document.getElementById("board");
-
-for (let i = 0; i < 49; i++) {
-  const cell = document.createElement("div");
-  cell.className = "cell";
-  board.appendChild(cell);
-}
